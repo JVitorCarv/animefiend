57839298ee5ddead7fa4c96b5bad5bccf83c2cee
Vitor Carvalho <vitor.velozo2003@gmail.com>
2023-08-02 10:22:10 -0300

    Add spinner

---
+++ components/Spinner/index.jsx
@@ -0,0 +1,22 @@
+import { Loader } from './styles'
+
+const Spinner = () => {
+  return (
+    <Loader>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </Loader>
+  )
+}
+
+export default Spinner
--- pages/Home/index.jsx
+++ pages/Home/index.jsx
@@ -4,6 +4,7 @@ import UploadBox from "../../components/UploadBox"
 import PictureBox from "../../components/PictureBox"
 import ListResult from "../../components/ListResult"
 import mockData from "../../mockData.json" /* remove later */
+import Spinner from "../../components/Spinner"
 
 const Home = () => {
     const [picture, setPicture] = useState()
@@ -14,8 +15,11 @@ const Home = () => {
         setPicture(pictureUrl)
     }
 
+    const [spinnerEnabled, setSpinnerEnabled] = useState(false)
     const [animeData, setAnimeData] = useState()
     const fetchData = async() => {
+        setAnimeData()
+        setSpinnerEnabled(true)
         if (picture) {
             const formData = new FormData()
             formData.append("image", file)
@@ -27,7 +31,8 @@ const Home = () => {
             const arrayData = dataJson.result.filter(data => data.anilist.isAdult === false)
             console.log(arrayData)
             setAnimeData(arrayData)
-        } 
+        }
+        setSpinnerEnabled(false)
     }
 
     return (
@@ -36,6 +41,7 @@ const Home = () => {
                 <s.Title>ANIMEFIeND🔍</s.Title>
                 {picture && (<PictureBox onClick={fetchData} picture={picture}/>)}
                 <s.ResultContainer>
+                    {spinnerEnabled && (<Spinner />)}
                     {animeData && animeData.map(data => (<ListResult key={data} data={data} />))}
                 </s.ResultContainer>
                 <UploadBox handleFileSelected={handleFileSelected}/>
